d877f4cb7eea3cd0d6823265efe4c4f3f47543aa
Igor Souza <souza.igor@protonmail.com>
2021-08-10 23:19:41 -0300

    feat : create discord client and connect

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
 
 namespace bot_discord
 {
@@ -6,7 +8,18 @@ namespace bot_discord
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync().GetAwaiter().GetResult();
+        }
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration() 
+            {
+                Token = Environment.GetEnvironmentVariable("BOT_TOKEN"),
+                TokenType = TokenType.Bot
+            });
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
         }
     }
 }
